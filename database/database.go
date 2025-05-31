04618d1b07d32ad8909416d8ee9d8ac4d9540e35
Samuel <marselinotambunan824@gmail.com>
2025-05-31 15:30:29 +0700

    Update database.go

--- database/database.go
+++ database/database.go
@@ -21,33 +21,51 @@ var (
 
 func getDB() *sql.DB {
 	dbOnce.Do(func() {
-		var err error
-		err = godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
+		// Hanya load .env di dev, Railway CLI inject vars otomatis
+		if err := godotenv.Load(); err != nil {
+			log.Println("No .env file found, relying on environment")
 		}
 
+		// Baca var MySQL dari Railway
+		user := os.Getenv("MYSQLUSER")
+		password := os.Getenv("MYSQLPASSWORD")
+		host := os.Getenv("MYSQLHOST")
+		// port := os.Getenv("MYSQLPORT")
+		name := os.Getenv("MYSQLDATABASE")
+
+		// Bangun DSN dengan format yang benar
+		// dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		//     user, password, host, port, name,
+		// )
+
+		var err error
+		// db, err = sql.Open("mysql", dsn)
+		// if err != nil {
+		//     log.Fatalf("sql.Open error: %v", err)
+		// }
+
 		db, err = sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true",
-			os.Getenv("DB_USERNAME"),
-			os.Getenv("DB_PASSWORD"),
-			os.Getenv("DB_URL"),
-			os.Getenv("DB_DATABASE")))
+			user,
+			password,
+			host,
+			name))
 		if err != nil {
 			log.Fatal(err)
 		}
-
+		// Optional: set pool size
 		db.SetMaxOpenConns(10)
 		db.SetMaxIdleConns(5)
-		err = db.Ping()
-		if err != nil {
+
+		if err = db.Ping(); err != nil {
 			log.Fatalf("Failed to connect to database: %v", err)
 		}
 
+		// Inisialisasi GORM
 		DB, err = gorm.Open(mysql.New(mysql.Config{
 			Conn: db,
 		}), &gorm.Config{})
 		if err != nil {
-			log.Fatal(err)
+			log.Fatalf("gorm.Open error: %v", err)
 		}
 	})
 	return db
